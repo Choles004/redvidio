99ffe6ea852d678b1a3ba1352d1a9c26a84c7d58
Choles004 <138090409+Choles004@users.noreply.github.com>
2024-11-20 13:45:46 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 // Esperar a que el DOM esté completamente cargado
 document.addEventListener('DOMContentLoaded', () => {
-    // Inicializar la aplicación
-    const storage = new Storage();
-    window.ui = new UI(storage); // Hacer ui accesible globalmente para los event handlers
+  // Inicializar la aplicación
+  const storage = new Storage();
+  window.ui = new UI(storage); // Hacer ui accesible globalmente para los event handlers
 });
