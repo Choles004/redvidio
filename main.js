e6ea37d8d376a1575f4ad03a45a7b3852f3a012b
Choles004 <138090409+Choles004@users.noreply.github.com>
2024-11-20 15:10:48 -0500

    Delete main.js

--- main.js
+++
@@ -1,6 +0,0 @@
-// Esperar a que el DOM esté completamente cargado
-document.addEventListener('DOMContentLoaded', () => {
-  // Inicializar la aplicación
-  const storage = new Storage();
-  window.ui = new UI(storage); // Hacer ui accesible globalmente para los event handlers
-});
